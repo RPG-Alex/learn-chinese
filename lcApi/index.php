62b6cb20627f6cc7f2162b0b17e1f14d8cff5f3f
alex blood <mralexblood@outlook.com>
2021-08-05 16:05:52 +0800

    started working on App API Backend

---
+++ lcApi/index.php
@@ -0,0 +1,17 @@
+<?php
+/*
+Things to do:
+Decide on API calls
+build db
+decide on API structure (probalby rest, whatever that is)
+build api
+???
+profit
+
+
+*/
+
+
+if (isset($_POST['login']) && $_POST['login'] != '') {
+  //do login stuff here
+} 
