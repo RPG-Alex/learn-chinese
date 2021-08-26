6196dbb9b74f3b5c77cfa8e990e8b580c9d4da2e
alex blood <mralexblood@outlook.com>
2021-08-26 15:07:08 +0800

    Can now Interact with API!

--- backend/index.php
+++ backend/index.php
@@ -2,11 +2,23 @@
 //Developer Tools!! Delete in production
 error_reporting( E_ALL);
 ini_set("display_errors",1);
-//
+//page header for API
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: OPTIONS,GET,POST,PUT,DELETE");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+//Import necesasry models for API
 include_once "model/Database.php";
 include_once "model/Lessons.php";
-
-$test = new Lessons;
-$result = $test->getVocabulary();
-$data = json_encode($result,JSON_UNESCAPED_UNICODE);
-var_dump($data);
+//If lesson is posted this will handle the input
+if ($_POST['lesson'] == 'flash') {
+  $test = new Lessons;
+  $result = $test->getVocabulary();
+  $data = json_encode($result,JSON_UNESCAPED_UNICODE);
+  echo $data;
+} elseif ($_POST['lesson'] > 0 ) {
+  echo "See me later";
+} else {
+  echo "You should not be seeing this";
+}
